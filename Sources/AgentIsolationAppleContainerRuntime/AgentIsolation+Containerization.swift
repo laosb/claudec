2fb712022895cce744b2ccdc61b8c6f73d738b45
laosb <git@lao.sb>
2026-04-08 17:19:47 +0800

    fix: gate Containerization code

--- Sources/AgentIsolationAppleContainerRuntime/AgentIsolation+Containerization.swift
+++ Sources/AgentIsolationAppleContainerRuntime/AgentIsolation+Containerization.swift
@@ -1,36 +1,34 @@
-import AgentIsolation
-import Containerization
-
-#if canImport(FoundationEssentials)
-  import FoundationEssentials
-#else
+#if canImport(Containerization)
+  import AgentIsolation
+  import Containerization
   import Foundation
+
+  struct ContainerizationReaderStream: Containerization.ReaderStream {
+    private let reader: any AgentIsolation.ReaderStream
+
+    init(_ reader: any AgentIsolation.ReaderStream) {
+      self.reader = reader
+    }
+
+    @inlinable func stream() -> AsyncStream<Data> {
+      reader.stream()
+    }
+  }
+
+  struct ContainerizationWriter: Containerization.Writer {
+    private let writer: any AgentIsolation.Writer
+
+    init(_ writer: any AgentIsolation.Writer) {
+      self.writer = writer
+    }
+
+    @inlinable func write(_ data: Data) throws {
+      try writer.write(data)
+    }
+
+    @inlinable func close() throws {
+      try writer.close()
+    }
+  }
+
 #endif
-
-struct ContainerizationReaderStream: Containerization.ReaderStream {
-  private let reader: any AgentIsolation.ReaderStream
-
-  init(_ reader: any AgentIsolation.ReaderStream) {
-    self.reader = reader
-  }
-
-  @inlinable func stream() -> AsyncStream<Data> {
-    reader.stream()
-  }
-}
-
-struct ContainerizationWriter: Containerization.Writer {
-  private let writer: any AgentIsolation.Writer
-
-  init(_ writer: any AgentIsolation.Writer) {
-    self.writer = writer
-  }
-
-  @inlinable func write(_ data: Data) throws {
-    try writer.write(data)
-  }
-
-  @inlinable func close() throws {
-    try writer.close()
-  }
-}
